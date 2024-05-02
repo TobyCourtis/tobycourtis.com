9487eb47eb1b3a2b6099bb7a25150c806cb5644b
tobycourtis <toby.courtis@hotmail.com>
2024-05-02 13:36:33 +0100

    ✨Completed initial about section layout

---
+++ src/About/About.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import profileImage from '../images/pp-square.png'
+import './About.css'
+
+
+function About() {
+    return (
+        <div className={"about-div"}>
+            <div className={"about-inner-div"}>
+                <div className={"left"}>
+                    <img src={profileImage} alt="profile-pic" className={'profile-image'}/>
+                </div>
+
+
+                <div className={"right"}>
+                    <div>
+                        <h1>Hi, I'm Toby!</h1>
+                        <p>Software Dev, currently training for an Ironman!</p>
+                        <br/>
+
+                        <p>Take a look below at some of my past and present projects ...</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
