42ca09a7fd769213057c4114a8fe775a45f9c1a3
tobycourtis <toby.courtis@hotmail.com>
2024-05-02 16:36:18 +0100

    ✨Made languages dynamic and added images

--- src/Languages/Languages.tsx
+++ src/Languages/Languages.tsx
@@ -1,10 +1,11 @@
 import './Languages.css'
 import * as React from 'react';
-import language from "../images/kotlin.png";
-import {experimentalStyled as styled} from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import {experimentalStyled as styled} from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+import kotlinImage from '../images/kotlin.png';
 
 const Item = styled(Paper)(({theme}) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -13,21 +14,35 @@ const Item = styled(Paper)(({theme}) => ({
     color: theme.palette.text.secondary,
 }));
 
-const Language = () => {
-    // TODO make the languages dynamic
+interface ILanguageProps {
+    name: string;
+    image: string;
+}
+
+const Language: React.FC<ILanguageProps> = ({name, image}) => {
     return (
         <div className={'language-div'}>
             <div className={'language-image-div language-inner-div'}>
-                <img src={language} id={'language-image'} alt={'language-image'}/>
+                <img src={image} id={'language-image'} alt={name}/>
             </div>
             <div className={'language-name-div language-inner-div'}>
-                <p id={'language-name'}>Kotlin</p>
+                <p id={'language-name'}>{name}</p>
             </div>
         </div>
     );
 };
 
 function Languages() {
+    const languages: ILanguageProps[] = [
+        {name: 'Kotlin', image: '/kotlin.png'},
+        {name: 'Python', image: '/python.png'},
+        {name: 'Java', image: '/java.png'},
+        {name: 'Typescript', image: 'typescript.png'},
+        {name: 'Javascript', image: '/javascript.png'},
+        {name: 'HTML/CSS', image: '/html.png'},
+        {name: 'SQL', image: '/sql.jpeg'},
+    ];
+
     return (
         <>
             <div>
@@ -38,11 +53,11 @@ function Languages() {
                 <div className={"languages-outer-div"}>
                     <div className={"languages-inner-div"}>
                         <Box sx={{flexGrow: 1}}>
-                            <Grid container rowSpacing={3} columnSpacing={{xs: 3}}>
-                                {Array.from(Array(9)).map((_, index) => (
+                            <Grid container rowSpacing={3} columnSpacing={{xs: 2}}>
+                                {languages.map((language, index) => (
                                     <Grid item xs={4} key={index}>
                                         <Item elevation={0} className={'item'}>
-                                            <Language/>
+                                            <Language name={language.name} image={language.image}/>
                                         </Item>
                                     </Grid>
                                 ))}
@@ -52,7 +67,7 @@ function Languages() {
                 </div>
             </div>
         </>
-    )
+    );
 }
 
 export default Languages;
