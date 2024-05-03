4809fc409a7d77dcbcdf5942cdbe6c0c34e9da86
tobycourtis <toby.courtis@hotmail.com>
2024-05-03 15:58:28 +0100

    ✨ Add links to my contact section

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -28,53 +28,65 @@ function contacts() {
                 <div className={"contacts-outer-div"}>
                     <div className={"contacts-inner-div"}>
                         <Box sx={{flexGrow: 1}}>
+
                             <Grid container rowSpacing={3} columnSpacing={{xs: 4}}>
-                                <Grid item xs={6} key={'foo'}>
+                                <Grid item xs={6} key={'email-item'}>
                                     <Item elevation={0} className={'item'}>
-                                        <div className={'contact-div'}>
-                                            <div className={'contact-image-div contact-inner-div'}>
-                                                <EmailIcon id={'icon'}/>
+                                        <a href={"mailto:toby.courtis@gmail.com"}>
+                                            <div className={'contact-div'}>
+                                                <div className={'contact-image-div contact-inner-div'}>
+                                                    <EmailIcon id={'icon'}/>
+                                                </div>
+                                                <div className={'contact-name-div contact-inner-div'}>
+                                                    <p id={'contact-name'}>EMAIL</p>
+                                                </div>
                                             </div>
-                                            <div className={'contact-name-div contact-inner-div'}>
-                                                <p id={'contact-name'}>EMAIL</p>
-                                            </div>
-                                        </div>
+                                        </a>
                                     </Item>
                                 </Grid>
-                                <Grid item xs={6} key={'foo'}>
+
+                                <Grid item xs={6} key={'linkedin-item'}>
                                     <Item elevation={0} className={'item'}>
-                                        <div className={'contact-div'}>
-                                            <div className={'contact-image-div contact-inner-div'}>
-                                                <LinkedInIcon id={'icon'}/>
+                                        <a href={"https://www.linkedin.com/in/tobycourtis/"}>
+                                            <div className={'contact-div'}>
+                                                <div className={'contact-image-div contact-inner-div'}>
+                                                    <LinkedInIcon id={'icon'}/>
+                                                </div>
+                                                <div className={'contact-name-div contact-inner-div'}>
+                                                    <p id={'contact-name'}>LINKEDIN</p>
+                                                </div>
                                             </div>
-                                            <div className={'contact-name-div contact-inner-div'}>
-                                                <p id={'contact-name'}>LINKEDIN</p>
-                                            </div>
-                                        </div>
+                                        </a>
                                     </Item>
                                 </Grid>
-                                <Grid item xs={6} key={'foo'}>
+
+                                <Grid item xs={6} key={'yt-item'}>
                                     <Item elevation={0} className={'item'}>
-                                        <div className={'contact-div'}>
-                                            <div className={'contact-image-div contact-inner-div'}>
-                                                <YouTubeIcon id={'icon'}/>
+                                        <a href={"https://www.youtube.com/user/TobyCourtis"}>
+                                            <div className={'contact-div'}>
+                                                <div className={'contact-image-div contact-inner-div'}>
+                                                    <YouTubeIcon id={'icon'}/>
+                                                </div>
+                                                <div className={'contact-name-div contact-inner-div'}>
+                                                    <p id={'contact-name'}>YOUTUBE</p>
+                                                </div>
                                             </div>
-                                            <div className={'contact-name-div contact-inner-div'}>
-                                                <p id={'contact-name'}>YOUTUBE</p>
-                                            </div>
-                                        </div>
+                                        </a>
                                     </Item>
                                 </Grid>
-                                <Grid item xs={6} key={'foo'}>
+
+                                <Grid item xs={6} key={'github-item'}>
                                     <Item elevation={0} className={'item'}>
-                                        <div className={'contact-div'}>
-                                            <div className={'contact-image-div contact-inner-div'}>
-                                                <GitHubIcon id={'icon'}/>
+                                        <a href={"https://github.com/TobyCourtis"}>
+                                            <div className={'contact-div'}>
+                                                <div className={'contact-image-div contact-inner-div'}>
+                                                    <GitHubIcon id={'icon'}/>
+                                                </div>
+                                                <div className={'contact-name-div contact-inner-div'}>
+                                                    <p id={'contact-name'}>GITHUB</p>
+                                                </div>
                                             </div>
-                                            <div className={'contact-name-div contact-inner-div'}>
-                                                <p id={'contact-name'}>GITHUB</p>
-                                            </div>
-                                        </div>
+                                        </a>
                                     </Item>
                                 </Grid>
                             </Grid>
