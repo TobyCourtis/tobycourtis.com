6deb498ec3284626eaad6016ec49d3f620cced2a
tobycourtis <toby.courtis@hotmail.com>
2024-05-03 14:37:23 +0100

    💄 Change about section text

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -13,8 +13,8 @@ function About() {
 
                 <div className={"right"}>
                     <div>
-                        <h1>Hi, I'm Toby!</h1>
-                        <p>Software Dev, currently training for an Ironman!</p>
+                        <h1>Hey, I'm Toby!</h1>
+                        <p>I'm a software dev, currently training for an Ironman!</p>
                         <br/>
 
                         <p>Take a look below at some of my past and present projects ...</p>
