722d573589a18483f0af830c90d14e915882715c
tobycourtis <toby.courtis@hotmail.com>
2024-06-01 09:45:22 +0100

    🐛 Fix unused Box component causing build failure

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import {experimentalStyled as styled} from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
 
 interface IProjectProp {
     title: string;
