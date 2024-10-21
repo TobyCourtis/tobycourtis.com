b90d0d0257e525084612f3f43396e9a55ce510a8
tobycourtis <toby.courtis@hotmail.com>
2024-10-21 19:19:28 +0100

    ✨ WIP first pass at tailwind grid

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -76,59 +76,98 @@ const Projects: React.FC = () => {
     ];
 
     return (
-        <div className="projects-outer">
-            <div className={'projects content'}>
-                <div>
-                    <h2 className={"section-title"}>Projects</h2>
-                </div>
-                <div className={"projects-content-outer-div"}>
-                    <div className={"projects-content-inner-div"}>
-                        <Grid container spacing={{xs: 6}}>
-                            {projects.map((project, index) => (
-                                <Grid item xs={4} key={index}>
-                                    <Item elevation={0} className={'item'}>
-                                        <Project
-                                            title={project.title}
-                                            description={project.description}
-                                            image={project.image}
-                                        />
-                                    </Item>
-                                </Grid>
-                            ))}
-                        </Grid>
+        <section>
+            <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+                <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
+                    <div className="grid place-content-center rounded bg-[#e5dcc347] p-6 sm:p-8">
+                        <div className="mx-auto max-w-md text-center lg:text-left">
+                            <header>
+                                <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">Projects</h2>
 
-                        <Grid container spacing={{xs: 6}} id={'project-highlight-grid'}>
-                            {highlightProj.map((project, index) => (
-                                <Grid item xs={8} key={index}>
-                                    <Item elevation={0} className={'item'}>
-                                        <Project
-                                            title={project.title}
-                                            description={project.description}
-                                            image={project.image}
-                                            className={'highlight-proj'}
-                                        />
-                                    </Item>
-                                </Grid>
-                            ))}
-                            <Grid item xs={4} key={'more'}>
-                                <Item elevation={0} className={'item'}>
-                                    {moreProjects.map((project, index) => (
-                                        <Project
-                                            title={project.title}
-                                            description={project.description}
-                                            image={project.image}
-                                        />
-                                    ))}
-                                </Item>
-                            </Grid>
-                        </Grid>
+                                <p className="mt-4 text-gray-500">
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas rerum quam amet
+                                    provident nulla error!
+                                </p>
+                            </header>
 
+                            <a
+                                href="#"
+                                className="mt-8 inline-block rounded border border-gray-900 bg-gray-900 px-12 py-3 text-sm font-medium text-white transition hover:shadow focus:outline-none focus:ring"
+                            >
+                                Shop All
+                            </a>
+                        </div>
+                    </div>
 
+                    <div className="lg:col-span-2 lg:py-8">
+                        <ul className="grid grid-cols-2 gap-4">
+                            <li>
+                                <a href="#" className="group block">
+                                    <img
+                                        src="/bully.png"
+                                        alt=""
+                                        className="aspect-video w-full rounded object-cover"
+                                    />
+
+                                    <div className="mt-3">
+                                        <h3
+                                            className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4"
+                                        >
+                                            Simple Watch
+                                        </h3>
+
+                                        <p className="mt-1 text-sm text-gray-700">$150</p>
+                                    </div>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#" className="group block">
+                                    <img
+                                        src="/trading.png"
+                                        alt=""
+                                        className="aspect-video w-full rounded object-cover"
+                                    />
+
+                                    <div className="mt-3">
+                                        <h3
+                                            className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4"
+                                        >
+                                            Simple Watch
+                                        </h3>
+
+                                        <p className="mt-1 text-sm text-gray-700">$150</p>
+                                    </div>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#" className="group block">
+                                    <img
+                                        src="/tth.png"
+                                        alt=""
+                                        className="aspect-video w-full rounded object-cover"
+                                    />
+
+                                    <div className="mt-3">
+                                        <h3
+                                            className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4"
+                                        >
+                                            Simple Watch
+                                        </h3>
+
+                                        <p className="mt-1 text-sm text-gray-700">$150</p>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
 
 export default Projects;
+
+
